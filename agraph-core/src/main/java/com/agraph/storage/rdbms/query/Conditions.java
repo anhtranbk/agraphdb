e0165beeb04785d1183d37b1a18ece18fbfe31a0
tjeubaoit <anhtn.bk@gmail.com>
2020-01-17 14:23:48 +0700

    Clean code

--- agraph-core/src/main/java/com/agraph/storage/rdbms/query/Conditions.java
+++ agraph-core/src/main/java/com/agraph/storage/rdbms/query/Conditions.java
@@ -2,53 +2,55 @@ package com.agraph.storage.rdbms.query;
 
 import java.util.Arrays;
 
+import static com.agraph.storage.rdbms.query.Condition.Operator;
+
 public class Conditions {
 
     public static SingleCondition eq(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.EQUALS, obj);
+        return new SingleCondition(col, Operator.EQUALS, obj);
     }
 
     public static SingleCondition neq(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.NOT_EQUALS, obj);
+        return new SingleCondition(col, Operator.NOT_EQUALS, obj);
     }
 
     public static SingleCondition lt(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.LESS, obj);
+        return new SingleCondition(col, Operator.LESS, obj);
     }
 
     public static SingleCondition lte(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.LESS_OR_EQUALS, obj);
+        return new SingleCondition(col, Operator.LESS_OR_EQUALS, obj);
     }
 
     public static SingleCondition gt(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.GREATER, obj);
+        return new SingleCondition(col, Operator.GREATER, obj);
     }
 
     public static SingleCondition gte(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.GREATER_OR_EQUALS, obj);
+        return new SingleCondition(col, Operator.GREATER_OR_EQUALS, obj);
     }
 
     public static SingleCondition isNull(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.IS_NULL, obj);
+        return new SingleCondition(col, Operator.IS_NULL, obj);
     }
 
     public static SingleCondition notNull(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.IS_NOT_NULL, obj);
+        return new SingleCondition(col, Operator.IS_NOT_NULL, obj);
     }
 
     public static SingleCondition in(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.IN, obj);
+        return new SingleCondition(col, Operator.IN, obj);
     }
 
     public static SingleCondition notIn(String col, Object obj) {
-        return new SingleCondition(col, Condition.Operator.NOT_IN, obj);
+        return new SingleCondition(col, Operator.NOT_IN, obj);
     }
 
     public static MultiCondition or(Condition... conditions) {
-        return new MultiCondition(Condition.Operator.OR, Arrays.asList(conditions));
+        return new MultiCondition(Operator.OR, Arrays.asList(conditions));
     }
 
     public static MultiCondition and(Condition... conditions) {
-        return new MultiCondition(Condition.Operator.AND, Arrays.asList(conditions));
+        return new MultiCondition(Operator.AND, Arrays.asList(conditions));
     }
 }
