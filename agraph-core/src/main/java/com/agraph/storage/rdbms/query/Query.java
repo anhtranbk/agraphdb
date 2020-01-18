5cc23a0103a46e907854d5e40360ee47dd4d7f3e
tjeubaoit <anhtn.bk@gmail.com>
2020-01-18 21:49:23 +0700

    Query support new two options: show deleted and show hidden fields

--- agraph-core/src/main/java/com/agraph/storage/rdbms/query/Query.java
+++ agraph-core/src/main/java/com/agraph/storage/rdbms/query/Query.java
@@ -19,15 +19,19 @@ public class Query {
     private final List<Order> orders;
     private final int offset;
     private final int limit;
+    private final boolean showDeleted;
+    private final boolean showHidden;
 
-    private Query(String table, Condition condition, List<String> columns,
-                  List<Order> orders, int offset, int limit) {
+    private Query(String table, Condition condition, List<String> columns, List<Order> orders,
+                  int offset, int limit, boolean showDeleted, boolean showHidden) {
         this.table = table;
         this.condition = condition;
         this.columns = columns;
         this.orders = orders;
         this.offset = offset;
         this.limit = limit;
+        this.showDeleted = showDeleted;
+        this.showHidden = showHidden;
     }
 
     public static Builder builder(String table) {
@@ -37,12 +41,14 @@ public class Query {
     @Accessors(fluent = true, chain = true)
     @Setter
     public static class Builder {
-        String table;
-        Condition condition;
-        List<String> columns = new ArrayList<>(8);
-        List<Order> orders = new ArrayList<>(8);
-        int offset = 0;
-        int limit = Integer.MAX_VALUE;
+        private String table;
+        private Condition condition;
+        private List<String> columns = new ArrayList<>(8);
+        private List<Order> orders = new ArrayList<>(8);
+        private int offset = 0;
+        private int limit = Integer.MAX_VALUE;
+        private boolean showDeleted = false;
+        private boolean showHidden = false;
 
         Builder(String table) {
             this.table = table;
@@ -62,7 +68,7 @@ public class Query {
             Preconditions.checkArgument(offset < 0, "Invalid offset %d", offset);
             Preconditions.checkArgument(limit < 0, "Invalid limit %d", limit);
 
-            return new Query(table, condition, columns, orders, offset, limit);
+            return new Query(table, condition, columns, orders, offset, limit, showDeleted, showHidden);
         }
     }
 }
